6f6aa6f5032559096d62f78645d66fb1635e4d6f
aslanchan7 <124756060+aslanchan7@users.noreply.github.com>
2025-03-28 17:08:11 -0700

    Created OrderManager singleton

--- Spring Game Jam 2025/Assets/Scripts/DragAndDrop.cs
+++ Spring Game Jam 2025/Assets/Scripts/DragAndDrop.cs
@@ -17,7 +17,6 @@ public class DragAndDrop : MonoBehaviour
         Vector2 mousePos = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         List<RaycastHit2D> hits = new();
         Physics2D.Raycast(mousePos, Vector2.zero, filter, hits);
-        Debug.Log(hits.Count);
         foreach (var hit in hits)
         {
             hit.collider.TryGetComponent<DragDroppable>(out var dragDropComponent);
--- Spring Game Jam 2025/Assets/Scripts/OrderManager.cs
+++ Spring Game Jam 2025/Assets/Scripts/OrderManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class OrderManager : MonoBehaviour
 {
+    public static OrderManager Instance;
+
     [Header("References")]
     [SerializeField] GameObject orderBoxPrefab;
     [SerializeField] GameObject ordersList;
@@ -12,17 +14,25 @@ public class OrderManager : MonoBehaviour
     [SerializeField] float orderInterval; // # seconds between each order
     [SerializeField] int maxOrderNum;
     private float lastOrderTime;
-    private List<Order> orders = new();
+    private float timeToNextOrder;
+    public List<Order> orders = new();
 
-    void Start()
+    void Awake()
     {
-
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
     }
 
     void Update()
     {
         // Randomly create orders
-        if (Time.time - lastOrderTime >= orderInterval && orders.Count < maxOrderNum)
+        if (timeToNextOrder <= 0f)
         {
             // Make an order
             Order order = GenerateRandomOrder();
@@ -30,9 +40,26 @@ public class OrderManager : MonoBehaviour
             // Instantiate Order Box
             CreateOrderBox(order);
 
-            lastOrderTime = Time.time;
+            timeToNextOrder = orderInterval;
             orders.Add(order);
         }
+
+        if (orders.Count < maxOrderNum)
+        {
+            timeToNextOrder -= Time.deltaTime;
+        }
+
+        // if (Time.time - lastOrderTime >= orderInterval && orders.Count < maxOrderNum)
+        // {
+        //     // Make an order
+        //     Order order = GenerateRandomOrder();
+
+        //     // Instantiate Order Box
+        //     CreateOrderBox(order);
+
+        //     lastOrderTime = Time.time;
+        //     orders.Add(order);
+        // }
     }
 
     private Order GenerateRandomOrder()
@@ -54,5 +81,7 @@ public class OrderManager : MonoBehaviour
         }
         orderBox.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = order.ClothingItem.ToString();
         orderBox.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = orderPatternText;
+
+        orderBox.GetComponent<OrderBox>().order = order;
     }
 }
