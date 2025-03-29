6d75f66fbb3bc026b1a4ee2929fec6504cb96884
Chase Peterson <cep.piano@gmail.com>
2025-03-28 17:08:02 -0700

    Reverted TrashCanScript

--- Spring Game Jam 2025/Assets/Scripts/TrashCanScript.cs
+++ Spring Game Jam 2025/Assets/Scripts/TrashCanScript.cs
@@ -2,24 +2,17 @@ using UnityEngine;
 
 public class TrashCanScript : MonoBehaviour
 {
+    public new Camera camera;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        camera = FindAnyObjectByType<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (InputManager.Instance.MouseClick.ReadValue<float>() != 0f)
-        {
-            Vector2 ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray, Vector2.zero);
-
-            if (hit.collider == gameObject)
-            {
-                Debug.Log("Mouse is over");
-            }
-        }
+        
     }
 }
